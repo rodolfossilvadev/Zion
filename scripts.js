3581bc6e466735cf3b54ca9a729df2fbaffec63c
rodolfossilvadev <rodolfodev30@gmail.com>
2024-08-18 10:16:06 -0300

    primeiro projeto zion

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+
+let formulario = document.querySelector(".formulario")
+
+ function aparecerformulario(){
+  formulario.style.left = "50%"
+ }
+ function desaparecerformulario(){
+formulario.style.left = "-50%"
+ }
